d75b9f73b193f9315ddba86a02b35414279cf9c9
Lara Petersen <laramarlenpetersen@gmail.com>
2018-11-27 22:24:25 +0100

    added cool styling, total score function and counter

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 window.onload = function() {
   //Hides all questions in the beginning:
-  $("#fromatob")
+  $(".fromatob")
     .children()
     .hide();
-  $("#clubscene")
+  $(".clubscene")
     .children()
     .hide();
-  $("#funfacts")
+  $(".funfacts")
     .children()
     .hide();
-  $("#eatingdrinking")
+  $(".eatingdrinking")
     .children()
     .hide();
-  $("#districtcliches")
+  $(".districtcliches")
     .children()
     .hide();
 
@@ -26,31 +26,31 @@ window.onload = function() {
     id: "berlinclubscene",
     x: 0,
     y: (Math.PI * 2) / 5,
-    color: "#2CEBA3"
+    color: "#498870"
   });
   categories.push({
     id: "fromatob",
     x: (Math.PI * 2) / 5,
     y: (2 * (Math.PI * 2)) / 5,
-    color: "#A32CEB"
+    color: "#704988"
   });
   categories.push({
     id: "eatingdrinking",
     x: (2 * (Math.PI * 2)) / 5,
     y: (3 * (Math.PI * 2)) / 5,
-    color: "#2CD3EB"
+    color: "#498088"
   });
   categories.push({
     id: "kiezclichees",
     x: (3 * (Math.PI * 2)) / 5,
     y: (4 * (Math.PI * 2)) / 5,
-    color: "#EB2CD3"
+    color: "#504988"
   });
   categories.push({
     id: "funfacts",
     x: (4 * (Math.PI * 2)) / 5,
     y: (5 * (Math.PI * 2)) / 5,
-    color: "#2C74EB"
+    color: "#884980"
   });
 
   var wheel = {
@@ -129,6 +129,7 @@ window.onload = function() {
   //Selects the category
 
   var selectedCategory;
+  var randomIndex;
 
   function selectCategory() {
     var remainderRotation = totalRotation % (Math.PI * 2);
@@ -144,12 +145,61 @@ window.onload = function() {
     console.log("------");
     categories.forEach(function(cat) {
       if (sector > cat.x && sector < cat.y) {
-        console.log(cat.id)
+        console.log(cat.id);
         selectedCategory = cat.id;
       }
     });
-    printCategory();
 
+    if (
+      selectedCategory === "fromatob" &&
+      $(".fromatob.notyetprinted").length > 0
+    ) {
+      randomIndex = Math.floor(
+        Math.random() * $(".fromatob.notyetprinted").length
+      );
+      selectedCategory = $($(".fromatob.notyetprinted")[randomIndex]);
+      printCategory();
+    } else if (
+      selectedCategory === "berlinclubscene" &&
+      $(".clubscene.notyetprinted").length > 0
+    ) {
+      randomIndex = Math.floor(
+        Math.random() * $(".berlinclubscene.notyetprinted").length
+      );
+      selectedCategory = $($(".clubscene.notyetprinted")[randomIndex]);
+      printCategory();
+    } else if (
+      selectedCategory === "eatingdrinking" &&
+      $(".eatingdrinking.notyetprinted").length > 0
+    ) {
+      randomIndex = Math.floor(
+        Math.random() * $(".eatingdrinking.notyetprinted").length
+      );
+      selectedCategory = $($(".eatingdrinking.notyetprinted")[randomIndex]);
+      printCategory();
+    } else if (
+      selectedCategory === "funfacts" &&
+      $(".funfacts.notyetprinted").length > 0
+    ) {
+      randomIndex = Math.floor(
+        Math.random() * $(".funfacts.notyetprinted").length
+      );
+      selectedCategory = $($(".funfacts.notyetprinted")[randomIndex]);
+      printCategory();
+    } else if (
+      selectedCategory === "kiezclichees" &&
+      $(".districtcliches.notyetprinted").length > 0
+    ) {
+      randomIndex = Math.floor(
+        Math.random() * $(".districtcliches.notyetprinted").length
+      );
+      selectedCategory = $($(".districtcliches.notyetprinted")[randomIndex]);
+      printCategory();
+    } else {
+      alert("There are no questions left in this category. Spin again!");
+      $("#spin").prop("disabled", false);
+      x = 10;
+    }
   }
   //other method: try to only work with remainderrotation for the case fun facts and mini part of district clichees? we know the remainderrotation is smaller than Math.PI*2/5 then it's funfacts and if it is bigger than that and smaller than Math.PI/180 * 90 it is kiezclichees
 
@@ -165,175 +215,173 @@ window.onload = function() {
   });
 
   //QUIZ
+
   //functions
 
   var index = 0;
-  var counter = 5;
-  var iterations = 5;
+  var countDown;
 
-
-
-  //Shows the first question of the category #fromatob:
+  //Shows the first question of the selected category:
   function printCategory() {
-
-    if (selectedCategory === "fromatob") {
-      selectedCategory = $("#fromatob");
-    }
-    if (selectedCategory === "berlinclubscene") {
-      selectedCategory = $("#clubscene");
-    }
-    if (selectedCategory === "eatingdrinking") {
-      selectedCategory = $("#eatingdrinking");
-    }
-    if (selectedCategory === "funfacts") {
-      selectedCategory = $("#funfacts");
-    }
-    if (selectedCategory === "kiezclichees") {
-      selectedCategory = $("#districtcliches");
-    }
-    
     $("#starttext").hide();
 
-      selectedCategory
-        .children()
-        .eq(index)
-        .show();
+    selectedCategory
+      .children()
+      .eq(index)
+      .show();
 
-        // var counter = setInterval(function() {
-        //   if (counter >= 0) {
-        //     $("#counter").text(counter);
-        //   } else {
-        //     questionWrong();
-        //     counter = 6;
-        //   }
-        //   counter--;
-        // }, 1000);
-    }
+    $("#counter").html(6);
 
-    //Adds a green point to the score bar, makes the question disappear and a new question appear:
-    function questionRight() {
-      var audio = new Audio("audiofile_wow.mp3");
-      audio.play();
-      $("#points")
-        .children()
-        .eq(index)
-        .addClass("greenpoint");
-        selectedCategory
-        .children()
-        .eq(index)
-        .hide();
-      gameOver();
-      index++;
-      selectedCategory
-        .children()
-        .eq(index)
-        .show();
-    }
-
-    //Adds a red point to the score bar, makes the question disappear and a new question appear:
-    function questionWrong() {
-      var audio = new Audio("audiofile_uhoh.mp3");
-      audio.play();
-      $("#points")
-        .children()
-        .eq(index)
-        .addClass("redpoint");
-        selectedCategory
-        .children()
-        .eq(index)
-        .hide();
-      gameOver();
-      index++;
-      selectedCategory
-        .children()
-        .eq(index)
-        .show();
-    }
-
-    //Checks if the answer that was clicked is the right answer or the wrong answer:
-    $(".answerSet input").click(function() {
-      if (
-        $(this)
-          .parent()
-          .hasClass("correct")
-      ) {
-        counter = 5;
-        questionRight();
-      } else {
-        counter = 5;
+    countDown = setInterval(function() {
+      if (parseFloat($("#counter").html()) === 0) {
         questionWrong();
+      } else {
+        $("#counter").html(parseFloat($("#counter").html()) - 1);
       }
-    });
+    }, 1000);
+  }
 
-    //Checks if all five questions have been answered and the game is over. Then alerts the final score:
-    function gameOver() {
-      if (index === 4) {
-        window.setTimeout(function() {
-          var counterRed = 0;
-          var counterGreen = 0;
-          var points = $(".points");
-          for (i = 0; i < points.length; i++) {
-            if (points.eq(i).hasClass("redpoint")) {
-              counterRed += 1;
-            }
-            if (points.eq(i).hasClass("greenpoint")) {
-              counterGreen += 1;
-            }
+  var totalScore = 0;
+
+  //Adds a green point to the score bar, makes the question disappear and a new question appear:
+  function questionRight() {
+    var audio = new Audio("audiofile_wow.mp3");
+    totalScore++;
+    audio.play();
+    $("#points")
+      .children()
+      .eq(index)
+      .addClass("greenpoint");
+    selectedCategory
+      .children()
+      .eq(index)
+      .hide();
+    roundOver();
+    index++;
+    selectedCategory
+      .children()
+      .eq(index)
+      .show();
+    $("#counter").html(6);
+    $("#totalscore").html("Total Score: " + totalScore)
+  }
+
+  //Adds a red point to the score bar, makes the question disappear and a new question appear:
+  function questionWrong() {
+    var audio = new Audio("audiofile_uhoh.mp3");
+    audio.play();
+    $("#points")
+      .children()
+      .eq(index)
+      .addClass("redpoint");
+    selectedCategory
+      .children()
+      .eq(index)
+      .hide();
+    roundOver();
+    index++;
+    selectedCategory
+      .children()
+      .eq(index)
+      .show();
+    $("#counter").html(6);
+  }
+
+  //Checks if the answer that was clicked is the right answer or the wrong answer:
+  $(".answerSet input").click(function() {
+    if (
+      $(this)
+        .parent()
+        .hasClass("correct")
+    ) {
+      questionRight();
+    } else {
+      questionWrong();
+    }
+  });
+
+  //Checks if all five questions have been answered and the game is over. Then alerts the final score:
+  function roundOver() {
+
+    if (index === 4) {
+      clearInterval(countDown);
+      window.setTimeout(function() {
+        var counterRed = 0;
+        var counterGreen = 0;
+        var points = $(".points");
+        for (i = 0; i < points.length; i++) {
+          if (points.eq(i).hasClass("redpoint")) {
+            counterRed += 1;
           }
-          if (counterRed === 0) {
-            alert(
-              "WOW! The game is over. You got " +
-                counterGreen +
-                " out of 5 questions right. You're a real Berliner!"
-            );
+          if (points.eq(i).hasClass("greenpoint")) {
+            counterGreen += 1;
           }
-          if (counterRed === 1) {
-            alert(
-              "The game is over. You got " +
-                counterGreen +
-                " out of 5 questions right. Awesome. "
-            );
-          }
-          if (counterRed === 2) {
-            alert(
-              "The game is over. You got " +
-                counterGreen +
-                " out of 5 questions right. Not bad."
-            );
-          }
-          if (counterRed === 3) {
-            alert(
-              "The game is over. You got " +
-                counterGreen +
-                " out of 5 questions right. You should try this again."
-            );
-          }
-          if (counterRed === 4) {
-            alert(
-              "The game is over. You got " +
-                counterGreen +
-                " out of 5 questions right. You should try this again. "
-            );
-          }
-          if (counterRed === 5) {
-            alert(
-              "Oh boy. The game is over. You got " +
-                counterGreen +
-                " out of 5 questions right. Loser!"
-            );
-          }
-          $("#starttext").show();
-          $("#spin").prop("disabled", false);
-          x = 10;
-          for (i = 0; i < points.length; i++) {
-          $("#points").children().eq(i).removeClass("greenpoint");
-          $("#points").children().eq(i).removeClass("redpoint");
-          index=0;
+        }
+
+        
+        if (counterRed === 0) {
+          alert(
+            "WOW! The round is over. You got " +
+              counterGreen +
+              " out of 5 questions right. You're a real Berliner!"
+          );
+        }
+        if (counterRed === 1) {
+          alert(
+            "The round is over. You got " +
+              counterGreen +
+              " out of 5 questions right. Awesome. "
+          );
+        }
+        if (counterRed === 2) {
+          alert(
+            "The round is over. You got " +
+              counterGreen +
+              " out of 5 questions right. Not bad."
+          );
+        }
+        if (counterRed === 3) {
+          alert(
+            "The round is over. You got " +
+              counterGreen +
+              " out of 5 questions right. You should try this again."
+          );
+        }
+        if (counterRed === 4) {
+          alert(
+            "The round is over. You got " +
+              counterGreen +
+              " out of 5 questions right. You should try this again. "
+          );
+        }
+        if (counterRed === 5) {
+          alert(
+            "Oh boy. The round is over. You got " +
+              counterGreen +
+              " out of 5 questions right. Loser!"
+          );
+        }
+        selectedCategory.removeClass("notyetprinted");
+        $("#starttext").show();
+        $("#spin").prop("disabled", false);
+        x = 10;
+        for (i = 0; i < points.length; i++) {
+          $("#points")
+            .children()
+            .eq(i)
+            .removeClass("greenpoint");
+          $("#points")
+            .children()
+            .eq(i)
+            .removeClass("redpoint");
+          index = 0;
           ctx.resetTransform();
           wheel.draw();
-          text.draw();}
-        }, 500);
-        // clearInterval(counter);
-      }
+          text.draw();
+        }
+        $("#counter").html(" ");
+      }, 500);
+      // clearInterval(counter);
     }
+  }
 };
